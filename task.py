32d9a4c797a503bba0f687403e4ca8a44f346a38
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-09-22 15:46:35 +0200

    Added task.py with `*args` and `**kwargs`

--- task.py
+++ task.py
@@ -1,5 +1,6 @@
-def func(a, z, *args):
-    print(args)
+def func(a, z, *args, **kwargs):
+    print(f"args: {args}")
+    print(f"kwargs: {kwargs}")
     a_sum = 0
     for item in args:
         a_sum += item
@@ -7,6 +8,7 @@ def func(a, z, *args):
 
 """
 args take all undefined elements, so here we don't get 2 and 3 displayed
+kwargs display all other arguments
 """
 
-print(func(2, 3, 4, 5, 6, 4233))
+print(func(2, 3, 4, 5, 6, 4233, v=34, s=43))
