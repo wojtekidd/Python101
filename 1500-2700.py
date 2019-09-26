384fb1b05c65a7bf3e87ab0e927bc98b5555a974
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-09-26 17:43:19 +0200

    Python homework 26 September 2019

--- 1500-2700.py
+++ 1500-2700.py
@@ -1,5 +1,5 @@
 iterator = 1500
-while(iterator <= 2700):
+while iterator <= 2700:
     if iterator % 5 == 0 and iterator % 7 == 0:
         print(iterator)
     iterator += 1
---
+++ leap_years.py
@@ -0,0 +1,18 @@
+def num_of_leap_years(year):
+
+return
+    while  % 4 == 0:
+        if n % 100 == 0:
+    elif n % 400 == 0
+
+    return 2019 - year
+
+
+"""
+If the year is evenly divisible by 4, go to step 2. ...
+If the year is evenly divisible by 100, go to step 3. ...
+If the year is evenly divisible by 400, go to step 4. ...
+The year is a leap year (it has 366 days).
+The year is not a leap year (it has 365 days).
+"""
+
