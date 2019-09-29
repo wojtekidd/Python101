7b5f6a0eabaa1336d7d5e0c4a0f6ba6937304952
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-09-29 22:06:12 +0200

    adding is_sleep_time

---
+++ is_sleep_time.py
@@ -0,0 +1,14 @@
+def is_sleep_time(weekday, vacation):
+
+    if weekday == :
+
+        return False
+    if vacation ==
+    else: return True
+
+
+"""
+is_sleep_time (False, False ) → True
+is_sleep_time (True, False ) → False
+is_sleep_time (False, True ) → True
+"""
