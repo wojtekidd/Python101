98db46a1e33f12935e3008dcf840e3b412185beb
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-09-30 14:42:04 +0200

    Python homework and new materials

--- all_primes.py
+++ all_primes.py
@@ -8,3 +8,10 @@ def all_primes(n):
 
 
 print(all_primes(20))
+
+"""
+W drugim zadaniu napisz najpierw funkcję, która sprawdza czy podana liczba jest pierwsza (is_prime(n)). 
+Do napisania tej funkcji będzie potrzbna pętla, która iteruje po kolejnych liczbach całkowitych (od 1 do n/2). 
+Jeżeli nasza liczba n będzie podzielna przez którąś z tych liczb wtedy n nie jest pierwsza. 
+Jeżeli liczba n nie będzie podzielna przez żadną z liczb od 1 do n/2 wtedy jest pierwsza.
+"""
