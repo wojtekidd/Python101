cfa75c8ae08e13cbb99610fd9f0c882ce60b707e
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-11-17 10:26:38 +0100

    Bubble sort algorithm and new Notebooks

--- algorithms.py
+++ algorithms.py
@@ -10,15 +10,15 @@ t = [4, 5, 6, 2, 22, 46, 34, 78, 9, 10]
 #     return x
 #
 # print(find_max(t))
-
-
-for counter in range(len(t)):
-    for i in range(len(t)-1):
-        if t[i] > t[i+1]:
-           temp = t[i]
-           t[i] = t[i+1]
-           t[i+1] = temp
-print(t)
+#
+#
+# for counter in range(len(t)):
+#     for i in range(len(t)-1):
+#         if t[i] > t[i+1]:
+#            temp = t[i]
+#            t[i] = t[i+1]
+#            t[i+1] = temp
+# print(t)
 
 def bubble_sort(array):
     for counter in range(len(array)):
@@ -30,3 +30,8 @@ def bubble_sort(array):
     return array
 
 print(bubble_sort([1,2,56,8,33]))
+
+element = 22
+for i in range(len(t)-1):
+    if t[i] == element:
+        print(i)
