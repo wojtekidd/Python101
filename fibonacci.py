54d76ce0faa19ef9ebe3d967b476b1405df7514c
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-11-17 11:43:59 +0100

    Fibonacci - two approaches

---
+++ fibonacci.py
@@ -0,0 +1,32 @@
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+
+
+# print(fib(20))
+
+def fib_iter(n):
+    a = 0
+    b = 1
+    if n == 0:
+        return a
+    if n == 1:
+        return b
+    if n >= 2:
+        for i in range(2, n):
+            temp = a
+            a = b
+            b = temp + a
+        result = a + b
+        return result
+
+print(fib_iter(2345))
+
+
+
+
+
