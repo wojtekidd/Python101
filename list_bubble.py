3282043914173c6c053aa3d66d8917b64cfcd533
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-11-23 11:25:27 +0100

    Working on bubble sorting lists

--- list_bubble.py
+++ list_bubble.py
@@ -7,4 +7,19 @@ class SLinkedList:
     def __init__(self):
         self.headval = None
 
+    def append(self, dataval=None):
+        tmp = self.headval
+        while tmp.nextval is not None:
+            tmp = tmp.nextval
+        tmp.nextval = Node(dataval)
+
+list = SLinkedList()  # 1
+node1 = Node("a")  # 2
+list.headval = node1  # 3
+node2 = Node("b")  # 4
+node1.nextval = node2  # 5
+
+tmp = list.headval
+
+
 
