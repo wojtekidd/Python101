e874f525a425ad113d0a4c12a9b6382ed580194a
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-11-23 15:53:44 +0100

    MIND BLOWN

--- list_bubble.py
+++ list_bubble.py
@@ -4,16 +4,26 @@ class Node:
         self.dataval = dataval
         self.nextval = None
 
+
     def __str__(self):
         return self.dataval
 
 
-class SLinkedList:
+class SLinkedList():
     def __init__(self):
         self.headval = Node()
 
-    def remove(selfself, dataval=None):
-        pass
+    def remove_last(self):
+        tmp = self.headval
+        if tmp is None:
+            pass
+        else:
+            while tmp.nextval is not None:  # Jesli istnieje nastepna wartosc po tej
+                tmp = tmp.nextval  # Przechodzi do nastepnej wartosci
+                if tmp.nextval.nextval is None: #  Dochodzimy do przedostatniego
+                    break
+            tmp.nextval = None
+
 
     def append(self, dataval=None):
         tmp = self.headval  # Pointing at the first Node
@@ -23,6 +33,19 @@ class SLinkedList:
             while tmp.nextval is not None:
                 tmp = tmp.nextval
             tmp.nextval = Node(dataval)
+
+    def addElement(self, count, val):
+        tmp = self.headval
+        if tmp is None and count == 0:
+            self.headval = Node(val)
+        else:
+            counter = 0
+            while counter < count - 1:  # pętla działa póki counter jest mniejszy niż zdefiniowana pozycja wybranej wartości
+                counter += 1
+                tmp = tmp.nextval
+            insert_this = Node(val)  # tworzy element o zdefiniowanej wartości val
+            insert_this.nextval = tmp.nextval  # przypisuje tmp.nextval do nextval nowego elementu
+            tmp.nextval = insert_this  # podmienia następujące po tmp wartości (czyli jego nextval) utworzonym elementem
 
     def printlist(self):
         tmp = self.headval
@@ -35,10 +58,22 @@ class SLinkedList:
                 tmp = tmp.nextval
 
 
+    def get_element(self,n):
+        counter = 0
+        tmp = self.headval
+        while tmp.nextval is not None:
+            tmp = tmp.nextval
+            counter += 1
+            if counter == n:
+                return tmp.nextval
+
+
 list = SLinkedList()
-#list.headval = Node("1")
-list.append("1")
+list.addElement(1,"w")
+list.addElement(1,"e")
+list.addElement(2,"r")
 list.printlist()
+
 
 
 
