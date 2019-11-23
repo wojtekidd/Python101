fc3a4c837225e63c82139ed207513b2e532d10f6
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-11-23 10:38:36 +0100

    Sorting algorithm - added print string for debugging

---
+++ list_bubble.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+
+
