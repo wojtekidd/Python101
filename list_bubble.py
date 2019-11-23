a3f5dddfcbf2688f29ee06fe86e69008d5e88811
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-11-23 13:41:49 +0100

    Printing list of objects

--- list_bubble.py
+++ list_bubble.py
@@ -1,25 +1,44 @@
 class Node:
+
     def __init__(self, dataval=None):
         self.dataval = dataval
         self.nextval = None
 
+    def __str__(self):
+        return self.dataval
+
+
 class SLinkedList:
     def __init__(self):
-        self.headval = None
+        self.headval = Node()
+
+    def remove(selfself, dataval=None):
+        pass
 
     def append(self, dataval=None):
+        tmp = self.headval  # Pointing at the first Node
+        if tmp is None:
+            self.headval = Node(dataval)
+        else:
+            while tmp.nextval is not None:
+                tmp = tmp.nextval
+            tmp.nextval = Node(dataval)
+
+    def printlist(self):
         tmp = self.headval
-        while tmp.nextval is not None:
+        if tmp.nextval is None:
+            print("The list is empty")
+        else:
             tmp = tmp.nextval
-        tmp.nextval = Node(dataval)
+            while tmp is not None:
+                print(tmp.dataval)
+                tmp = tmp.nextval
 
-list = SLinkedList()  # 1
-node1 = Node("a")  # 2
-list.headval = node1  # 3
-node2 = Node("b")  # 4
-node1.nextval = node2  # 5
 
-tmp = list.headval
+list = SLinkedList()
+#list.headval = Node("1")
+list.append("1")
+list.printlist()
 
 
 
