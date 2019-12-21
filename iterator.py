775edad003652efa7bcbc0fad4496f275b1d62ba
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-12-21 15:52:04 +0100

    Having fun with iterator

---
+++ iterator.py
@@ -0,0 +1,14 @@
+my_list = [1, 2, 3, 4]
+
+# for item in my_list:
+#     print(item)
+
+my_iterator = iter(my_list)
+while True:
+    try:
+        item = next(my_iterator)
+        print(item)
+    except StopIteration:
+        break
+
+print(iter(my_iterator))
