36a12a2bce5cc79cff653afa9d00e28b5e1e9188
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-12-21 12:14:00 +0100

    Introduced a good and a bad class.

--- abstract_interface.py
+++ abstract_interface.py
@@ -11,3 +11,24 @@ class MyABC(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def some_property(self):
         """Required property"""
+
+
+class MyClass(MyABC):
+    def __init__(self, value=None):
+        self._my_prop = value
+
+    def do_something(self, value):
+        self._my_prop *= 2 + value
+
+    @property
+    def some_property(self):
+        return self._my_prop
+
+
+class BadClass(MyABC):
+    pass
+
+
+my_class = MyClass()
+print(my_class)
+bad_class = BadClass()
