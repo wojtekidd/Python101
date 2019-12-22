84362e3da052897426c0e8998424296b1df1cbfe
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-12-22 12:44:48 +0100

    Working on a tickets dashboard with abstract classes

--- proper_observer.py
+++ proper_observer.py
@@ -2,3 +2,13 @@ from dashboard.abs_observer import AbsObserver
 
 
 class Observer(AbsObserver):
+    _open_tickets = 0
+    _close_tickets = 0
+    _assigned_tickets = 0
+
+    def __init__(self, subject):
+        self._subject = subject
+        subject.attach(self)
+
+    def detach(self, subject):
+        subject.detach(self)
