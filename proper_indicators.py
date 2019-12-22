74be98754de35118e81a56a7eb2e89086619ae2f
Wojtek Cichon <wojciech.cichon@gmail.com>
2019-12-22 12:59:13 +0100

    Working on a tickets dashboard with abstract classes

--- proper_indicators.py
+++ proper_indicators.py
@@ -6,9 +6,20 @@ class Indicator(AbsSubject):
     _closed_ticket = 0
     _assigned_ticket = 0
 
+    @property
+    def open_tickets(self):
+        return self._open_tickets
 
-def set_indicators(self, open_tickets, closed_tickets, assigned_tickets):
-    self._open_tickets = open_tickets
-    self._closed_tickets = closed_tickets
-    self._assigned_tickets = assigned_tickets
-    self.notify()
+    @property
+    def closed_tickets(self):
+        return self.closed_tickets
+
+    @property
+    def assigned_tickets(self):
+        return self._assigned_tickets
+
+    def set_indicators(self, open_tickets, closed_tickets, assigned_tickets):
+        self._open_tickets = open_tickets
+        self._closed_tickets = closed_tickets
+        self._assigned_tickets = assigned_tickets
+        self.notify()
--- proper_observer.py
+++ proper_observer.py
@@ -3,7 +3,7 @@ from dashboard.abs_observer import AbsObserver
 
 class Observer(AbsObserver):
     _open_tickets = 0
-    _close_tickets = 0
+    _closed_tickets = 0
     _assigned_tickets = 0
 
     def __init__(self, subject):
@@ -12,3 +12,14 @@ class Observer(AbsObserver):
 
     def detach(self, subject):
         subject.detach(self)
+
+    def update(self):
+        self._open_tickets = self._subject.open_tickets
+        self._closed_tickets = self._subject.closed_tickets
+        self._assigned_tickets = self._subject.assigned_tickets
+        self.display()
+
+    def display(self):
+        print(f"Current open tickets:{self._open_tickets}")
+        print(f"Current closed tickets:{self._closed_tickets}")
+        print(f"Current assigned tickets:{self._assigned_tickets}\n")
