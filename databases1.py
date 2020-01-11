b6911eb263983bf0276d927c692781dbee0e34af
Wojtek Cichon <wojciech.cichon@gmail.com>
2020-01-11 12:36:32 +0100

    Programmiing databases - pandas and matplotlib

--- databases1.py
+++ databases1.py
@@ -1,9 +1,34 @@
 import sqlalchemy as db
+import pandas as pd
+import matplotlib.pyplot as plt
 
-engine = db.create_engine("mysql+mysqlconnector://root:foobar@localhost:3306/sqlalchemy_mysql")
+engine = db.create_engine('mysql+mysqlconnector://root:foobar@localhost:3306/sqlalchemy_mysql')
 connection = engine.connect()
 
-query = 'SELECT * FROM tags'
-result = engine.execute(query).first()
+query = 'SELECT ViewCount, AnswerCount FROM posts where ViewCount is not NULL'
 
-print(result)
+# result = engine.execute(query).fetchall()
+# print(result)
+
+posts_df = pd.read_sql_query(query, engine)
+# print(posts_df)
+# print('---')
+# print(posts_df.columns)
+# print('---')
+# print(posts_df.dtypes)
+# print('---')
+# print(posts_df.head())
+# print('---')
+# print(posts_df[['ViewCount', 'AnswerCount']].max())
+# print('---')
+# print(posts_df[['ViewCount', 'AnswerCount']].describe())
+# print('---')
+
+x = posts_df['AnswerCount']
+y = posts_df['ViewCount']
+plt.scatter(x, y)
+plt.ylim(0, 12000)
+plt.xlabel('Answers')
+plt.ylabel('Views')
+plt.title('Posts: Views vs. Answers')
+plt.show()
