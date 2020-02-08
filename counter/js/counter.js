20861697568259ced87d3676a0b362946ca0ecc7
Wojtek Cichon <wojtek.cichon@protonmail.com>
2020-02-08 14:49:19 +0100

    jS increment

--- counter/js/counter.js
+++ counter/js/counter.js
@@ -2,5 +2,13 @@ let c = 0;
 
 function increment() {
     c++;
-    document.querySelector("h1").innerHTML = c
+    document.querySelector("h1").innerHTML = c;
+
+    if (c % 10 === 0) {
+        alert(`The counter value is ${c}`);
+    }
+
+    if (c == 5) {
+        alert(`The counter equals 5`);
+    }
 }
