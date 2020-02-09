685962760fa7a6683a990cdf62ce5a52f0b97e74
Wojtek Cichon <wojtek.cichon@protonmail.com>
2020-02-09 12:45:03 +0100

    localStorage in JS

---
+++ localStorage/js/event1.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("button").onclick = increment;
+});
+
+localStorage.getItem('counter');
+
+let c = 0;
+
+function increment() {
+    c++;
+    document.querySelector("h1").innerHTML = c;
+
+    if (c % 10 === 0) {
+        alert(`The counter value is ${c}`);
+    }
+
+    if (c == 5) {
+        alert(`The counter equals 5`);
+    }
+}
